af6c02f077c6396d53a6b5e5433204bcd3432e35
logicamechanism <m.quinn.p@protonmail.com>
2021-11-20 12:01:07 -0800

    Updated graphing information and rewrote the readme.

--- README.md
+++ README.md
@@ -2,25 +2,55 @@
 
 A python script to perform an origin trace on a cnft.
 
-## How to use
+![alt text](nx_html_image.png)
 
-To create an html page use the create_html_page function.
+## What is this graph?
 
-```py
-create_html_page(policy_id: str, asset_name:str, smart_contract_address:str="", print_flag:bool=True)
-```
+This is a directed graph where each node is a unique wallet and each colored edge is a transaction going between unique wallets. The darker edges represent either a withdraw and sell action. Each color is defined by the amount of unique wallets so if the colors are equal then its the same wallet
 
-This will produce and attempt to open a file called nx.html in the local directory.
-
-### Example
-
-Directly from the terminal:
+### Requirements
+This file requires a mainnet Blockfrost api key.
 
 ```bash
-python origin_trace.py 8634f3bf5cd864c4b661ff25789ae0154b34084d431c222d242bc39c DEADTRAILLOGIC11
+# Requires a mainnet Blockfrost api key.
+echo ${BLOCKFROST_API_KEY} >> blockfrost_api.key
 ```
 
-### What this graphs shows
+Python 3.9+, networkX, and pyVis will be needed.
 
-This returns a directed graph where each node is a unique wallet and each edge is a transaction going between unique wallets. Each color is defined by the amount of unique wallets so if the colors are equal then its the same wallet.
+```bash
+pip install -r requirements.txt
+```
 
+## How to use
+
+Import origin trace and use the create html page function. This will create a nx.html file inside the local folder.
+
+```py
+from origin_trace import create_html_page
+policy_id              = "8634f3bf5cd864c4b661ff25789ae0154b34084d431c222d242bc39c"
+asset_name             = "DEADTRAILLOGIC11"
+smart_contract_address = "addr1wyl5fauf4m4thqze74kvxk8efcj4n7qjx005v33ympj7uwsscprfk"
+create_html_page(
+    policy_id,
+    asset_name,
+    smart_contract_address,
+    print_flag=True,
+    save_flag=True
+)
+```
+
+If print is set to true, it will display the information inside the terminal and attempt to open nx.html. 
+
+If save is set to true then it will just save the nx.html file to the local folder and it will save all the addresses and transactions into a json file, cnft_history.json.
+
+This file can also be used directly from the terminal.
+
+```bash
+python origin_trace.py \
+8634f3bf5cd864c4b661ff25789ae0154b34084d431c222d242bc39c \
+DEADTRAILLOGIC11
+```
+
+
+.
--- origin_trace.py
+++ origin_trace.py
@@ -119,6 +119,24 @@ def track_asset(policy_id: str, asset_name:str, smart_contract_address:str="") -
     G = build_graph(addresses, smart_contract_address)
     return G, addresses
 
+def find_node(G, val):
+    """
+    Return True if a vertex exists within G else False.
+    """
+    return any([node for node in G.nodes(data=True) if node[0] == val])
+
+def analyze_trajectory(G:nx.classes.digraph.DiGraph) -> nx.classes.digraph.DiGraph:
+    for node in G.nodes(data=True):
+        (n, data) = node
+        if data['label'] == 'Contract' and find_node(G, n+1) is True:
+            a = G.nodes(data=True)[n-1]['address']
+            b = G.nodes(data=True)[n+1]['address']
+            if a == b:
+                G.add_edge(n-1, n+1, trxHash="Withdraw", title="Withdraw", label="Withdraw", color="#000000", alpha=0.54)
+            else:
+                G.add_edge(n-1, n+1, trxHash="Sold", title="Sold", label="Sold", color="#000000", alpha=0.54)
+    return G
+
 
 def print_address_data(addresses: list):
     """
@@ -151,29 +169,43 @@ def create_html_page(policy_id: str, asset_name:str, smart_contract_address:str=
     """
     print('\nTracking Asset...\n')
     G, addresses = track_asset(policy_id, asset_name, smart_contract_address)
+    G = analyze_trajectory(G)
     print('Creating HTML page')
     nt = Network('100%', '100%', heading=policy_id+'.'+asset_name, directed=True)
     nt.from_nx(G)
     if print_flag is True:
         print_address_data(addresses)
         nt.show('nx.html')
-    if save_flag is True:
+    elif save_flag is True:
         save_address_data(addresses)
         nt.save_graph('nx.html')
+    else:
+        print("No flag is set.")
     print('\nComplete!\n')
 
 
 if __name__ == "__main__":
+    """
+    Change the parameters to desired use case.
+    """
+    # Define flags
+    print_flag = False
+    save_flag  = True
+    
     # Pass in policy id and asset name.
     args = sys.argv
     try:
-        policy_id = args[1]
+        policy_id  = args[1]
         asset_name = args[2]
     except IndexError:
         print('Missing policy id or asset name. The format should be:\n\npython origin_trace.py policy_id asset_name\n')
         sys.exit()
+    
+    # Optional smart contract or address arg
     if len(args) == 4:
         smart_contract_address = args[3]
     else:
         smart_contract_address = "addr1wyl5fauf4m4thqze74kvxk8efcj4n7qjx005v33ympj7uwsscprfk"
-    create_html_page(policy_id, asset_name, smart_contract_address, False, True)
+    
+    #
+    create_html_page(policy_id, asset_name, smart_contract_address, print_flag, save_flag)
